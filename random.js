fe641002d6cc5e0b921e03dad639c30eac00f0e1
rwarasaurus <info@kieronwilson.co.uk>
2012-08-29 11:06:40 +0100

    init commit

---
+++ random.js
@@ -0,0 +1,33 @@
+
+var Random = function() {
+
+	var alpha = 'abcdefghijklmnopqrstuvwxyz', num = '0123456789';
+
+	var shuffle = function(o) {
+		for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+		return o;
+	};
+
+	var repeat = function(input, multiplier) {
+		return new Array(multiplier + 1).join(input);
+	};
+
+	var rand = function(length, pool) {
+		var arr = shuffle(repeat(pool, 5).split(''));
+
+		return arr.slice(0, length).join('');
+	};
+
+	return {
+		'alpha': function(length) {
+			return rand(length, alpha);
+		},
+		'alnum': function(length) {
+			return rand(length, alpha + num);
+		},
+		'num': function(length) {
+			return rand(length, num);
+		}
+	};
+
+}();
---
+++ readme.md
@@ -0,0 +1,5 @@
+## Random.js
+
+Generate random strings and numbers
+
+	Random.alnum(10); // b5tts8025r
